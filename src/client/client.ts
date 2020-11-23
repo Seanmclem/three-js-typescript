9e70006827888ed84dd938958f05a47732e5623a
seanmclem <seanmclements@gmail.com>
2020-11-22 22:57:39 -0500

    add guyi and stats

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,7 @@
-import { worker } from 'cluster'
 import * as THREE from '/build/three.module.js'
 import { OrbitControls } from '/jsm/controls/OrbitControls'
+import Stats from '/jsm/libs/stats.module'
+import {GUI} from '/jsm/libs/dat.gui.module'
 
 const scene: THREE.Scene = new THREE.Scene()
 
@@ -8,14 +9,10 @@ const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.i
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-const render = () => {
-    renderer.render(scene, camera)
-}
-
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-// controls.addEventListener("change", render)
+controls.addEventListener('change', render) 
 
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
@@ -25,22 +22,41 @@ scene.add(cube)
 
 camera.position.z = 2
 
-const onWindowResize = () => {
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
-window.addEventListener("resize", onWindowResize, false)
 
-let animate = function () {
+const stats = Stats()
+document.body.appendChild(stats.dom)
+const gui = new GUI();
+const cubeFolder = gui.addFolder("Cube")
+cubeFolder.add(cube.rotation, "x", 0, Math.PI*2, 0.01)
+cubeFolder.add(cube.rotation, "y", 0, Math.PI*2, 0.01)
+cubeFolder.add(cube.rotation, "z", 0, Math.PI*2, 0.01)
+// cubeFolder.open()
+const cameraFolder = gui.addFolder("Camera")
+cameraFolder.add(camera.position, "z", 0, 10, 0.01)
+// cameraFolder.open()
+
+var animate = function () {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
 
     render()
+
+    stats.update()
 };
 
-animate() //// looping
-//render() ////one time
+function render() {
+    //stats.begin()
+    renderer.render(scene, camera)
+    //stats.end()
+}
+//render()
+animate();
